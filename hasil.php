8a569825e2cf61534ccc03d784d5118d1e473b07
athayapurnama02 <115131302+athayapurnama02@users.noreply.github.com>
2023-10-17 23:32:28 +0800

    Update hasil.php

--- hasil.php
+++ hasil.php
@@ -4,24 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles2.css">
-    <title>Hasil Biodata</title>
+    <title>List of Biodata</title>
 </head>
 <body>
     <div class="container">
         <div class="topbar">
             <a href="form_biodata.html">>> Back to Form <<</a>
         </div>
-        <h1>Hasil Biodata</h1>
+        <h1>List of Biodata</h1>
+
         <?php
+        include 'koneksi.php';
+
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // Mengambil data yang dikirimkan melalui form
             $nama = $_POST["nama"];
             $umur = $_POST["umur"];
             $jk = $_POST["jk"];
             $tanggal = $_POST["tanggal"];
             $alamat = $_POST["alamat"];
             $pendidikan = $_POST["pendidikan"];
-            $hobi = isset($_POST["hobi"]) && is_array($_POST["hobi"]) ? implode(", ", $_POST["hobi"]) : "";
+            $hobi = $_POST["hobi[]"];
             $email = $_POST["email"];
             $password = $_POST["password"];
             $telepon = $_POST["telepon"];
@@ -29,24 +31,34 @@
             $warna = $_POST["warna"];
             $nilai = $_POST["nilai"];
 
-            // Menampilkan data dalam tabel
-            echo '<table>';
-            echo '<tr><th>Field</th><th>Value</th></tr>';
-            echo "<tr><td>Nama</td><td>$nama</td></tr>";
-            echo "<tr><td>Umur</td><td>$umur tahun</td></tr>";
-            echo "<tr><td>Jenis Kelamin</td><td>$jk</td></tr>";
-            echo "<tr><td>Tanggal Lahir</td><td>$tanggal</td></tr>";
-            echo "<tr><td>Alamat</td><td>$alamat</td></tr>";
-            echo "<tr><td>Pendidikan Terakhir</td><td>$pendidikan</td></tr>";
-            echo "<tr><td>Hobi</td><td>$hobi</td></tr>";
-            echo "<tr><td>Email</td><td>$email</td></tr>";
-            echo "<tr><td>Password</td><td>**** (tidak ditampilkan)</td></tr>";
-            echo "<tr><td>Telepon</td><td>$telepon</td></tr>";
-            echo "<tr><td>Waktu Senggang</td><td>$waktu</td></tr>";
-            echo "<tr><td>Warna Favorit</td><td><div class='color-box' style='background-color: $warna;'></div></td></tr>";
-            echo "<tr><td>Nilai</td><td>$nilai</td></tr>";
-            echo '</table>';
+            $sql = "INSERT INTO tb_biodata (nama, umur, jk, tanggal, alamat, pendidikan, hobi, email, password, telepon, waktu, warna, nilai) 
+                    VALUES ('$nama', '$umur', '$jk', '$tanggal', '$alamat', '$pendidikan', '$hobi', '$email', '$password', '$telepon', '$waktu', '$warna', '$nilai')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Data telah berhasil dimasukkan ke dalam database.";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
         }
+
+        $sql = "SELECT nama, umur FROM tb_biodata";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo '<table>';
+            echo '<tr><th>Nama</th><th>Umur</th><th>Action</th></tr>';
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr><td>{$row['nama']}</td><td>{$row['umur']}</td>";
+                echo "<td><a href='detail.php?id={$row['id']}'>Detail</a> | ";
+                echo "<a href='edit.php?id={$row['id']}'>Edit</a> | ";
+                echo "<a href='delete.php?id={$row['id']}'>Delete</a></td></tr>";
+            }
+            echo '</table>';
+        } else {
+            echo "No data available.";
+        }
+
+        $conn->close();
         ?>
     </div>
 </body>
