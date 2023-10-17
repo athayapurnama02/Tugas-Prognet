528c5cb3e54d87cbb61d1372c1b98f4053ab15ad
athayapurnama02 <115131302+athayapurnama02@users.noreply.github.com>
2023-10-18 02:04:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -21,24 +21,8 @@ if (!/^[A-Za-z\s]+$/.test(nama)) {
 }
 
 var umur = document.getElementById("umur").value;
-var jk = document.querySelector('input[name="jk"]:checked').value;
 var tanggal = document.getElementById("tanggal").value;
 var alamat = document.getElementById("alamat").value;
-var pendidikan = document.getElementById("pendidikan").value;
-var hobi = [];
-var hobiCheckboxes = document.querySelectorAll('input[name="hobi[]"]:checked');
-hobiCheckboxes.forEach(function(checkbox) {
-    hobi.push(checkbox.value);
-});
-
-// validasi kolom email (harus mengandung @)
-var email = document.getElementById("email").value;
-if (!email.includes('@')) {
-    alert('Kolom Email harus mengandung simbol "@".');
-    return false;
-}
-
-var password = document.getElementById("password").value;
 
 // validasi kolom no telepon (hanya angka)
 var telepon = document.getElementById("telepon").value;
@@ -46,10 +30,6 @@ if (!/^\d+$/.test(telepon)) {
     alert('Kolom No Telepon hanya bisa menginput angka.');
     return false;
 }
-
-var waktu = document.getElementById("waktu").value;
-var warna = document.getElementById("warna").value;
-var nilai = document.getElementById("nilai").value;
 
 // Jika semua validasi sukses, form akan dikirim
 return true; 
