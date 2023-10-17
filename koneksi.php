6b541eac8198c2df27bad38d712878d6d26d6835
athayapurnama02 <115131302+athayapurnama02@users.noreply.github.com>
2023-10-17 23:31:42 +0800

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "192.168.4.4";
+$username = "2205551027";
+$password = "2205551027";
+$dbname = "db_2205551027";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    echo "Connected successfully";
+}
+?>
