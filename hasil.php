aa608ee1393c164618b285d24dd9d048887b4b73
athayapurnama02 <115131302+athayapurnama02@users.noreply.github.com>
2023-10-18 01:46:47 +0800

    Update hasil.php

--- hasil.php
+++ hasil.php
@@ -14,6 +14,9 @@
         <h1>List of Biodata</h1>
 
         <?php
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+
         include 'koneksi.php';
 
         if (isset($_POST['insert'])) {
